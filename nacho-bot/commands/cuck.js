dd33fe37a45073cdc18eb1fd0b3cf43ca6f198bd
Jakub Chaba <35639052+jnchaba@users.noreply.github.com>
2020-12-28 23:30:20 -0500

    delete unused / depracated command

--- nacho-bot/commands/cuck.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name:'cuck',
-    description:'cuck',
-    execute(message, args) {
-        message.reply('I don\'t know, tell <@194630142029594625> to set this command!');
-    }
-}
