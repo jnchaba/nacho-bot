e63d402f34a505c844d2654a990136a9ba21b067
jnchaba <jakub.chaba@btopenworld.com>
2021-01-11 16:40:36 -0500

    removed unused command

--- nacho-bot/commands/yakub.js
+++
@@ -1,9 +0,0 @@
-const { MessageAttachment } = require('discord.js');
-
-module.exports = {
-    name:'yakub',
-    description:'yakub',
-    execute(message, args) {
-        message.channel.send(new MessageAttachment("./images/yakoob.jpg"));
-    }
-}
