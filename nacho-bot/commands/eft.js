556801c879a7446fafff850deaf92e9278e62fb3
jnchaba <jakub.chaba@btopenworld.com>
2021-01-22 11:02:28 -0500

    added prototype scrape argument

--- nacho-bot/commands/eft.js
+++ nacho-bot/commands/eft.js
@@ -1,9 +1,28 @@
 const { MessageAttachment } = require('discord.js');
+const Discord = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
     name:'eft',
     description:'eft',
     execute(message, args) {
+        if (args[0] === "scrape") {
+            var data = fs.readFileSync('../marketdata.json', 'utf-8');
+            var words = JSON.parse(data);
+            var count = Object.keys(words).length;
+            const scrapeEmbed = new Discord.MessageEmbed()
+                .setColor('#ff9933')
+                .setTitle('EFT Tarkov-Data Scrape Results')
+                .setAuthor('nacho-bot', 'https://cdn.discordapp.com/app-icons/769781677747863592/fd1ed280e50b3f16bc401dd698b8096b.png?size=256')
+                .setDescription("Number of items scraped: " + count);
+            for (let index = 0; index < count; index++) {
+                    scrapeEmbed.addField('Item:', words[index].name);
+                    scrapeEmebed.addField('Avg Price:', word[index].avg);
+                }
+            scrapeEmbed.setTimestamp();
+            scrapeEmbed.setFooter('Command currently being developed', 'https://cdn.discordapp.com/emojis/713166932538556499.png?v=');
+            message.channel.send(scrapeEmbed);
+        }
         if (args[0] === "customs") {
             message.channel.send(new MessageAttachment("./images/customs_map.png"));
         }
